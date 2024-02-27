de0e0c0a9476403ef3687364af80bc7bdeed4810
hoangnamit2004 <hoangnamit23032004@gmail.com>
2024-02-27 14:29:21 +0700

    To tinh html

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Love You",
+    introTitle: 'Your Crush',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Click vào đây..',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Delay....chat',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
+    mess: 'Đợi chút mình đang mơ mà...',
+    messDesc: 'Đi choi game day.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/61namit/'
+}
